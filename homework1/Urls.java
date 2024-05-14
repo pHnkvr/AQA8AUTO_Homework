a7e8bbf393284be2dd0d5564d29dcdad64049cfb
pHnkvr <rezuqqqqq@gmail.com>
2024-05-14 11:22:09 +0300

    test commit

---
+++ homework1/Urls.java
@@ -0,0 +1,10 @@
+package homework1;
+
+public class Urls {
+
+        public static final String zooKiev = "https://zoo.kiev.ua/";
+        public static final String w3schools = "https://www.w3schools.com/";
+        public static final String taxi838 = "https://taxi838.ua/ru/dnepr/";
+        public static final String klopotenko = "https://klopotenko.com/";
+
+}
