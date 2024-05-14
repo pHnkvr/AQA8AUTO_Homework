0a5f034bfd703dcf6160b684635fa12e237bbea6
pHnkvr <rezuqqqqq@gmail.com>
2024-05-14 12:05:17 +0300

    test commit

--- homework1/task3/printInfoAboutElements.java
+++ homework1/task3/printInfoAboutElements.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-public class printInfoAboutIDOfElement {
+public class printInfoAboutElements {
     public static void printInfoAbout(WebElement element){
         String elementId = element.getAttribute("id");
         String tagName = element.getTagName();
