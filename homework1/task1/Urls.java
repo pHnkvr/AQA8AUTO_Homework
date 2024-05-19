1fcf5914c98453d289a14473c23ff16116700e16
pHnkvr <rezuqqqqq@gmail.com>
2024-05-19 22:52:42 +0300

    test commit

--- homework1/task1/Urls.java
+++ homework1/task1/Urls.java
@@ -9,6 +9,9 @@ public class Urls {
         public static final String shopSite = "http://www.automationpractice.pl/index.php";
         public static final String TextBox = "http://only-testing-blog.blogspot.com/2014/01/textbox.html?";
         public static final String siteForDragNDrop="https://demo.guru99.com/test/drag_drop.html";
+        public static final String googleSearchPage = "https://www.google.com/search";
+        public static final String guinnessWorldRegister = "https://www.guinnessworldrecords.com/account/register?";
+        public static final String secondSearchFind = "https://www.hyrtutorials.com/p/alertsdemo.html";
 
 
 }
